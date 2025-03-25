672d8fb05b483b8fa76cea52be0a41fc7b0a3f14
Rafael <84077010+Rifuzada@users.noreply.github.com>
2025-03-24 22:02:31 -0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -8,10 +8,14 @@
 		-- Select | Update | Insert
 	--DCL Ling de controle de dados
 		--Grant | Revoke
-		 
+-- comando para excluir um banco de dados
+
+--drop database transporteJoao
 
 create database transporteJoao
-
+go
+use transporteJoao
+go
 create table veiculo
 (
 	placa varchar(7) primary key,
@@ -19,7 +23,7 @@ create table veiculo
 	ano int null,
 	cor varchar(100) null,
 )
-
+go
 create table motorista
 (
 	cod_motorista int identity(1,1) primary key,
@@ -28,7 +32,7 @@ create table motorista
 	telefone varchar(15) not null,
 	cnh varchar(200),
 )
-
+go
 create table passageiro
 (
 	cod_passageiro int identity(1,1) primary key,
@@ -37,7 +41,7 @@ create table passageiro
 	email varchar(30) not null,
 	telefone varchar(15) not null,
 )
-
+go
 create table manutencao
 (
 	cod_manutencao int identity(1,1) not null primary key,
@@ -45,4 +49,39 @@ create table manutencao
 	nome varchar(200) not null,
 	placa varchar(7) not null references veiculo(placa),
 
+)
+go
+create table viagem
+(
+	cod_viagem int identity(1,1) not null primary key,
+	rua_origem varchar(100) not null,
+	numero_origem varchar(10),
+	bairro_origem varchar(200),
+	estado_origem varchar(10),
+	cep_origem varchar(10),
+	rua_destino varchar(100),
+	numero_destino varchar(10),
+	bairro_destino varchar(200),
+	estado_destino varchar(200),
+	cep_destino varchar(10),
+	valor numeric(10,2) not null,
+	data_saida datetime not null,
+	data_retorno datetime,
+	cod_motorista int not null references motorista(cod_motorista),
+	placa varchar(7) not null references veiculo(placa),
+)
+go
+create table carga
+(
+	cod_carga int identity(1,1) primary key,
+	nome varchar(50) not null,
+	quantidade int not null,
+	cod_viagem int not null references viagem(cod_viagem),
+)
+go
+create table passsageiro_viagem
+(
+	cod_viagem int not null references viagem(cod_viagem),
+	cod_passageiro int not null references passageiro(cod_passageiro),
+	primary key (cod_viagem, cod_passageiro),
 )
